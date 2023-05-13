bd2aca47683188dacb3ce59de9d873d75c3c64d3
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-13 21:13:59 +0800

    adding some GUI

--- TestTV.py
+++ TestTV.py
@@ -1,3 +1,7 @@
+#Import the necessary module
+import tkinter as tk
+import tkinter.font as tkFont
+
 #Import the TV file
 from TV import TV
 
@@ -19,9 +23,24 @@ class TestTV:
         tv2.setChannel(3)
         tv2.setVolume(2)
 
-        #Display the outputs
-        print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
-        print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())
+        #Output strings for TV1 and TV2
+        output1 = f"tv1's channel is {tv1.getChannel()} and volume level is {tv1.getVolume()}"
+        output2 = f"tv2's channel is {tv2.getChannel()} and volume level is {tv2.getVolume()}" 
+
+        #Create the root window
+        root = tk.Tk()
+        root.title("Output Window")
+        root.geometry("400x100")
+
+        #Create a custom font object
+        font = tkFont.Font(family = "Century Gothic", size = 12)
+
+        #Create a label widget to display the output
+        label = tk.Label(root, text = output1 + "\n" + output2, font = font, width = 500, height = 200, bg = "black", fg = "yellow")
+        label.pack()
+
+        #Start the tkinter event loop
+        root.mainloop()
 
 #Execute the main method of the TestTV class
 TestTV().main()
