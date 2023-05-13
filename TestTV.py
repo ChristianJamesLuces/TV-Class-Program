bdc2f5b207031e70d46803959c43f24c99863874
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-13 16:43:35 +0800

    Pseuducode

--- TestTV.py
+++ TestTV.py
@@ -1,5 +1,17 @@
 class TV:
     def __init__(self):
+        # Default values for channel, volumelevel, and TV state
         self.channel = 1
         self.volumelevel = 1
         self.on = False
+
+        # Turn on the TV
+        # Turn off the TV
+        # Get the current channel
+        # Set a new channel
+        # Gets the new volume level
+        # Increases the channel number by 1
+        # Decreases the channel number by 1
+        # Increases the volume level by 1
+        # Decreases the volume level by 1
+
