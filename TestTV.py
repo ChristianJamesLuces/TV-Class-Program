b5929f85f4b6e9d2ec4e3bf89341c6045a9c5e6d
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-13 17:38:56 +0800

    Pseuducode for TestTV program

--- TestTV.py
+++ TestTV.py
@@ -1,1 +1,8 @@
-
+#Import the TV file
+#Define the TestTV class
+#Main method to test the class
+#Create two TV objects
+#Turn on TV1 and set its channel and volume level
+#Turn on TV2 and set its channel and volume level
+#Display the outputs
+#Execute the main method of the TestTV class
