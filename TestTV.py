ba9bb06b00e6c1dc1bb3ede1d99baa7fde06f571
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-13 16:46:28 +0800

    turn on the TV

--- TestTV.py
+++ TestTV.py
@@ -1,17 +1,19 @@
 class TV:
+    #Default values for channel, volumelevel, and TV state
     def __init__(self):
-        # Default values for channel, volumelevel, and TV state
         self.channel = 1
         self.volumelevel = 1
         self.on = False
 
-        # Turn on the TV
-        # Turn off the TV
-        # Get the current channel
-        # Set a new channel
-        # Gets the new volume level
-        # Increases the channel number by 1
-        # Decreases the channel number by 1
-        # Increases the volume level by 1
-        # Decreases the volume level by 1
+    #Turn on the TV
+    def turnOn(self):
+        self.on = True
+    #Turn off the TV
+    #Get the current channel
+    #Set a new channel
+    #Gets the new volume level
+    #Increases the channel number by 1
+    #Decreases the channel number by 1
+    #Increases the volume level by 1
+    #Decreases the volume level by 1
 
