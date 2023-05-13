85035b4f047581ef0f8d96b3a7447d526470e01a
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-13 18:47:36 +0800

    define the TestTV class

--- TestTV.py
+++ TestTV.py
@@ -1,7 +1,8 @@
 #Import the TV file
 from TV import TV
-#Define the TestTV class
 
+#Define the TestTV class
+class TestTV:
 #Main method to test the class
 #Create two TV objects
 #Turn on TV1 and set its channel and volume level
